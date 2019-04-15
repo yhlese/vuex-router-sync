77b4293c21943e38dd5678b14cab0cb3b388c3ed
yhlese <yhlese@qq.com>
2019-04-15 09:38:19 +0800

    [feat] : 动态事件名:

--- src/components/main/assets/task/index.vue
+++ src/components/main/assets/task/index.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="task">
-    这是任务页面
+    <p>动态事件名:</p>
+    <el-input v-model="value"
+              @[inputEvent]="doSomething"
+              style="width:100px"
+              placeholder="请输入内容"></el-input>
+    <el-button @click="()=>{ this.inputEvent = 'focus'}"
+               v-if="inputEvent === 'input'">切换事件为Input</el-button>
+    <el-button @click="()=>{ this.inputEvent = 'input';}"
+               v-else>切换事件为Focus</el-button>
+
+    <div id="obj1"
+         @click.self="doc">
+      obj1
+      <div id="obj2"
+           @click="doc">
+        obj2
+        <div id="obj3"
+             @click="doc">
+          obj3
+          <div id="obj4"
+               @click="doc">
+            obj4
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 <script>
@@ -8,7 +34,23 @@ import { getList } from '@/api/task/index.js';
 process.env.NODE_ENV === 'development' &&
   require('../../../../mockJs/task/index.js');
 export default {
+  data() {
+    return {
+      value: '',
+      // 动态事件名
+      inputEvent: 'input'
+    };
+  },
   methods: {
+    doc() {
+      this.id = event.currentTarget.id;
+      console.log(this.id);
+    },
+    // input执行事件
+    doSomething() {
+      console.log('val', this.value);
+    },
+    // mock数据获取
     async fetchData() {
       try {
         let data = await getList();

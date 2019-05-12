58e0d7734742cb7dcc766a1a4755fe5550b5db07
yhlese <yhlese@qq.com>
2019-05-12 12:10:48 +0800

    feat：v-slot

--- src/components/main/assets/task/index.vue
+++ src/components/main/assets/task/index.vue
@@ -26,19 +26,45 @@
         </div>
       </div>
     </div>
-
+    <!-- test: v-for 经常报warn:key值 -->
+    <div v-for="(item,index) in warnForKeyList" :key="index">{{item}}-{{index}}</div>
+    <!-- test: this.$set 添加多个属性 -->
+    <el-button @click="setMore">添加多个属性</el-button> 
+    <!-- test: v-slot -->
+    <v-slots>
+      <template v-slot:header="slotProps">
+        <div>
+          header-name：{{slotProps.slotInfo.name}}
+        </div>
+      </template>
+      <p>main</p>
+      <template v-slot:footer>
+        <div>
+          footer
+        </div>
+      </template>
+      <p>copy</p>
+    </v-slots>
   </div>
 </template>
 <script>
 import { getList } from '@/api/task/index.js';
 process.env.NODE_ENV === 'development' &&
   require('../../../../mockJs/task/index.js');
+import VSlots from '@/components/main/assets/task/v-slot.vue'
 export default {
+  components:{
+    VSlots
+  },
   data() {
     return {
       value: '',
       // 动态事件名
-      inputEvent: 'input'
+      inputEvent: 'input',
+      warnForKeyList:{
+        a:'a',
+        b:'b'
+      }
     };
   },
   methods: {
@@ -58,6 +84,16 @@ export default {
       } catch (error) {
         console.log('error', error);
       }
+    },
+    // 给 data 添加新的值
+    setMore(){
+      // 添加多个
+      this.warnForKeyList = Object.assign({},this.warnForKeyList,{
+        age:12,
+        sex:'男'
+      })
+      // 只添加一个属性 this.warnForKeyList.name = 'Liriye' 添加不成功的  
+      this.$set(this.warnForKeyList,'name','Liriye')
     }
   },
   mounted() {
---
+++ src/components/main/assets/task/v-slot.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- v-slot -->
+    <div>
+        <header>
+            <slot name="header" v-bind:slotInfo="slotInfo">
+            </slot>
+        </header>
+        <main>
+            <slot></slot>
+        </main>
+        <footer>
+            <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            slotInfo:{
+                name:'这是slot里面的name'
+            }
+        }
+    }
+}
+</script>
